c19fd0fd754e2646ca0857e5cf429bd70bdedd03
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-06-28 17:02:48 +0300

    Remove redundant modificators

--- contracts/RegistrationManageable.sol
+++ contracts/RegistrationManageable.sol
@@ -10,20 +10,9 @@ contract RegistrationManageable is RegistrationData, FeeHelper {
     event PoolCreationPriceChanged(uint256 NewPrice, uint256 OldPrice);
     event PoolCreationTokenChanged(address NewToken, address OldToken);
 
-    modifier isCorrectTokenAddress(address _token) {
-        require(_token != address(0), "Invalid token address.");
-        _;
-    }
-
-    modifier isCorrectPrice(uint256 _price) {
-        require(_price >= 0, "Price must be equal or greater than zero.");
-        _;
-    }
-
     function SetTokenPoolCreation(address _token)
         external
         onlyOwnerOrGov
-        isCorrectTokenAddress(_token)
     {
         address oldToken = BaseFee.FeeToken();
         SetToken(_token);
@@ -34,7 +23,6 @@ contract RegistrationManageable is RegistrationData, FeeHelper {
     function SetPoolCreationPrice(uint256 _price)
         external
         onlyOwnerOrGov
-        isCorrectPrice(_price)
     {
         uint256 oldPrice = BaseFee.Fee();
         SetFee(_price);
