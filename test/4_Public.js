278138a83300095d4537e12ba17108b9dd4a6e78
Dmytro Podelnik <dmitrypodelnik.developer@outlook.com>
2022-07-13 14:37:46 +0300

    update issue34

--- test/4_Public.js
+++ test/4_Public.js
@@ -38,7 +38,8 @@ contract("Admin settings", accounts => {
             const pools = await instance.RegistrationPools(poolId);
             const companiesAmount = pools[3].toNumber();
             console.log(pools);
-            const valuesAmount = await instance.GetValues(poolId, 2);
+            console.log(companiesAmount);
+            const valuesAmount = await instance.GetValues(0, 2);
             const result = await instance.SignUpPools(companiesAmount - 1);
             assert.equal(5, valuesAmount.length);
         });
