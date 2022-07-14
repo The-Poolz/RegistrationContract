3b5697d9570db0b768a120f19489a4e4f10ed1df
Dmytro Podelnik <dmitrypodelnik.developer@outlook.com>
2022-07-14 15:05:15 +0300

    Remove redundant rows

--- contracts/RegistrationUser.sol
+++ contracts/RegistrationUser.sol
@@ -57,13 +57,6 @@ contract RegistrationUser is RegistrationPO {
         isCorrectPoolId(_poolId)
         returns (uint256[] memory)
     {
-        address[] memory owners = new address[](
-            RegistrationPools[_poolId].UserSignUps
-        );
-        for (uint256 i = 0; i < RegistrationPools[_poolId].UserSignUps; i++) {
-            owners[i] = RegistrationPools[_poolId].SignUpPools[i].Owner;
-        }
-
         return
             _findMatchingValues(
                 RegistrationPools[_poolId].UserSignUps,
